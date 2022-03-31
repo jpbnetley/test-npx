351b24bef0892f0012a411b34c3a6ad64b8ac334
Jonathan Netley <jpbnetley@gmail.com>
2022-04-01 01:10:23 +0200

    feat: setup npm rc

--- bin/cliTest.js
+++ bin/cliTest.js
@@ -2,9 +2,12 @@
 const homedir = require('os').homedir();
 const fs = require("fs")
 const prompt = require("prompt-sync")({ sigint: true });
+const { setupNpmRc } = require('./util')
 
 const NPM_RC_FILE_PATH = `${homedir}/.npmrc`
 const doesNpmRcExist = fs.existsSync(NPM_RC_FILE_PATH)
+
+
 
 console.log("This script will generate an .npmrc file.")
 console.log('doesNpmRcExist', doesNpmRcExist)
@@ -15,26 +18,22 @@ if (doesNpmRcExist) {
   console.log('2. append');
   const userInput = prompt('Please enter your selection: ');
 
+  //TODO: user input does not log out
   console.log('userInput', userInput);
 
-  // switch (userInput) {
-  //   case '1':
-  //     console.log('overriding');
-  //     break;
-  //   case '2':
-  //     console.log('appending');
-  //     break;
-  //   default:
-  //     console.log("nothing selected");
-  //     break;
-  // }
+  switch (userInput) {
+    case '1':
+      setupNpmRc({ path: NPM_RC_FILE_PATH })
+      break;
+    case '2':
+      setupNpmRc({ path: NPM_RC_FILE_PATH, append: true })
+      break;
+    default:
+      console.log("nothing selected, will now exit");
+      process.exit(0)
+  }
 }
 
-// const userPat = prompt("please enter your personal access token: ")
-
-// if (!userPat) {
-//   console.log('no input was entered, will now exit')
-//   process.exit(0)
-// }
+console.log(`.npmrc file is created at: ${NPM_RC_FILE_PATH}`)
 
 process.exit(0)
